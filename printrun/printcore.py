6968effa9900aadddedc1da7fac43016919fe8ce
Wee Kiam Peng <wee.kiampeng@orangeknob.com>
2014-05-28 16:16:09 +0800

    portabee Go connect without pressing reset

--- printrun/printcore.py
+++ printrun/printcore.py
@@ -205,6 +205,13 @@ class printcore():
             self.read_thread.start()
             self._start_sender()
 
+            """ portabee Go connect with DTR """
+            self.printer.setDTR(0)
+            time.sleep(0.1)
+            self.printer.setDTR(1)
+
+
+
     def reset(self):
         """Reset the printer
         """
